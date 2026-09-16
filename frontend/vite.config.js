bb57e88bcfe1ea56d21a35f00008debb116efe42
Gabriela Tomaz <gabrielatomaz1772@gmail.com>
2026-09-15 22:16:34 -0300

    vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [vue()],
-  base: mode === 'production' ? '/first-assist/' : '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-}))
+  base: '/'
+})
